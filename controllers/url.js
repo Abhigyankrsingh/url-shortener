d24d0f5e6195ebb747c78cdc9f7a0771cf6bb72c
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 21:59:32 +0530

    Code_Updated

--- controllers/url.js
+++ controllers/url.js
@@ -5,6 +5,15 @@ const URL = require('../models/url');
 
 const URL = 
 async function handlegenerateNewShortURL(req,res){
-  const shortID = nanoid(8);
+  const body = req.body;
 
+  if(!body.url) return res.status(400).json({error: 'url is required'})
+  
+  
+    const shortID = nanoid(8);
+   await URL.create({
+    shortID: shortID,
+    redirectURL: body.url,
+    visitHistory : [],
+   });
 }
