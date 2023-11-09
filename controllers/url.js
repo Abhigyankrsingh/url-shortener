e80f44db6ac0ab87f41c143ebf51f51fbf5a74a5
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 21:52:16 +0530

    Code_Updated

--- controllers/url.js
+++ controllers/url.js
@@ -1,4 +1,10 @@
-async function handlegenerateNewShortURL(req,res){
+const { nanoid } = require("nanoid");
 
-    
+const URL = require('../models/url');
+
+
+const URL = 
+async function handlegenerateNewShortURL(req,res){
+  const shortID = nanoid(8);
+
 }
